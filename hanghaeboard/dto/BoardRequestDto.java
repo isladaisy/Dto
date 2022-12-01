0d0c1864715021d47cc2519b54dce352f2c3c3ee
isladaisy <117756400+isladaisy@users.noreply.github.com>
2022-12-01 11:32:48 +0900

    Add files via upload
    Spring 개인과제

---
+++ hanghaeboard/dto/BoardRequestDto.java
@@ -0,0 +1,14 @@
+package com.example.hanghaeboard.dto;
+
+import lombok.Getter;
+
+@Getter
+
+public class BoardRequestDto {
+
+    private String title;
+    private String contents;
+    private String author;
+    private String password;
+
+}
